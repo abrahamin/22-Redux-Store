311f067100c2f21674163901f19b23415d5cfb7d
Abraham Hong <hhmfjg@gmail.com>
2021-11-25 04:06:24 -0500

    redux dependencies

---
+++ client/build/precache-manifest.01237bfc01fb4b4ea0d94a5943cda8f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970d1a96acdc636f8f93f3704b52a870",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c6f66273719423bc568",
+    "url": "/static/css/main.6816d842.chunk.css"
+  },
+  {
+    "revision": "57d65bd1a116a2c36759",
+    "url": "/static/js/2.e0512a7f.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e0512a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6f66273719423bc568",
+    "url": "/static/js/main.7e2cd398.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
